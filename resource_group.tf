5d2e2349c9df54c2f1db26b4c1fe55683dd2a1a4
FouadDevOps <algahmif@aetna.com>
2024-05-03 16:54:28 -0400

    update

--- resource_group.tf
+++ resource_group.tf
@@ -2,12 +2,3 @@ resource "azurerm_resource_group" "aks_rg" {
   name     = "aks_resource_group"
   location = "East US"
 }
-
-
-output "aks_rg_name" {
-  value = azurerm_resource_group.aks_rg.name
-}
-
-output "aks_rg_location" {
-  value = azurerm_resource_group.aks_rg.location
-}
