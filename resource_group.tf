4f3d58b6cf2f3c205fe4739cedfa24e5fce97c76
FouadDevOps <algahmif@aetna.com>
2024-05-03 14:21:54 -0400

    add rg group

--- resource_group.tf
+++ resource_group.tf
@@ -2,12 +2,3 @@ resource "azurerm_resource_group" "aks_rg" {
   name     = "aks_resource_group"
   location = "East US"
 }
-
-data "azurerm_resource_group" "aks_rg" {
-  name                 = "aks_resource_group"
-
-}
-
-output "rg_id" {
-  value = data.azurerm_resource_group.aks_rg
-}
