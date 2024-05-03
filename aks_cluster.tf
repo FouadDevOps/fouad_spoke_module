32d27f386646940d42a43337e8d608d38d782ade
FouadDevOps <algahmif@aetna.com>
2024-05-03 08:36:14 -0400

    add prefix

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -2,6 +2,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.aks_cluster.name
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix = "fouad"
 
   default_node_pool {
     name       = var.node_count
