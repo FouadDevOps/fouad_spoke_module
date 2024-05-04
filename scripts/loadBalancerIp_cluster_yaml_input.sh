c96ce09b2d9b308539358ed3c84250bb42549b10
FouadDevOps <algahmif@aetna.com>
2024-05-04 01:57:08 -0400

    fix push

--- scripts/loadBalancerIp_cluster_yaml_input.sh
+++ scripts/loadBalancerIp_cluster_yaml_input.sh
@@ -95,6 +95,7 @@ if ls | grep -q $CLUSTER_NAME.yaml; then
   if git status | grep -q "Changes to be committed"; then
     echo "Committing and pushing changes..."
     git commit -m "$COMMIT_ACTION loadBalancerIp flag - $GITHUB_RUN_ID"
+    git pull origin $BRANCH_NAME
     git push -u origin $BRANCH_NAME
     gh pr create --fill
     gh pr merge $BRANCH_NAME --admin --squash
