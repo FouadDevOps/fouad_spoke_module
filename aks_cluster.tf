03f5af77ecf8f09fe2179879128d848e15667960
FouadDevOps <algahmif@aetna.com>
2024-05-07 11:06:48 -0400

    add service mesh script

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -62,41 +62,41 @@ resource "null_resource" "loadBalancerIp" {
 }
 
 
-# # Null Resource for Service Mesh Configuration
-# resource "null_resource" "service_mesh" {
-#   count = var.aks_cluster.service_mesh != null || var.aks_cluster.service_mesh == "istio" ? 1 : 0
+# Null Resource for Service Mesh Configuration
+resource "null_resource" "service_mesh" {
+  count = var.aks_cluster.service_mesh != null || var.aks_cluster.service_mesh == "istio" ? 1 : 0
 
-#   triggers = {
-#     cluster_name   = azurerm_kubernetes_cluster.aks_cluster.name
-#     service_mesh   = var.aks_cluster.service_mesh
-#     loadBalancerIp = var.aks_cluster.loadBalancerIp
-#   }
+  triggers = {
+    cluster_name   = azurerm_kubernetes_cluster.aks_cluster.name
+    service_mesh   = var.aks_cluster.service_mesh
+    loadBalancerIp = var.aks_cluster.loadBalancerIp
+  }
 
-#   provisioner "local-exec" {
-#     when        = create
-#     working_dir = "${path.module}/scripts"
-#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh add $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
-#     environment = {
-#       CLUSTER_NAME     = self.triggers.cluster_name
-#       SERVICE_MESH     = self.triggers.service_mesh
-#       LOAD_BALANCER_IP = self.triggers.loadBalancerIp
-#       RESOURCE_GROUP   = self.triggers.resource_group_name
-#       VNET_NAME        = self.triggers.vnet_name
-#       SUBNET_NAME      = self.triggers.subnet_name
-#     }
-#   }
+  provisioner "local-exec" {
+    when        = create
+    working_dir = "${path.module}/scripts"
+    command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh add $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
+    environment = {
+      CLUSTER_NAME     = self.triggers.cluster_name
+      SERVICE_MESH     = self.triggers.service_mesh
+      LOAD_BALANCER_IP = self.triggers.loadBalancerIp
+      RESOURCE_GROUP   = self.triggers.resource_group_name
+      VNET_NAME        = self.triggers.vnet_name
+      SUBNET_NAME      = self.triggers.subnet_name
+    }
+  }
 
-#   provisioner "local-exec" {
-#     when        = destroy
-#     working_dir = "${path.module}/scripts"
-#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh rm $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
-#     environment = {
-#       CLUSTER_NAME     = self.triggers.cluster_name
-#       SERVICE_MESH     = self.triggers.service_mesh
-#       LOAD_BALANCER_IP = self.triggers.loadBalancerIp
-#       RESOURCE_GROUP   = self.triggers.resource_group_name
-#       VNET_NAME        = self.triggers.vnet_name
-#       SUBNET_NAME      = self.triggers.subnet_name
-#     }
-#   }
-# }
+  provisioner "local-exec" {
+    when        = destroy
+    working_dir = "${path.module}/scripts"
+    command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh rm $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
+    environment = {
+      CLUSTER_NAME     = self.triggers.cluster_name
+      SERVICE_MESH     = self.triggers.service_mesh
+      LOAD_BALANCER_IP = self.triggers.loadBalancerIp
+      RESOURCE_GROUP   = self.triggers.resource_group_name
+      VNET_NAME        = self.triggers.vnet_name
+      SUBNET_NAME      = self.triggers.subnet_name
+    }
+  }
+}
