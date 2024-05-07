c1b1220c5d39f55353c6b553b693b89483761320
FouadDevOps <algahmif@aetna.com>
2024-05-07 09:44:16 -0400

    fix name

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -50,7 +50,7 @@ resource "null_resource" "loadBalancerIp" {
   provisioner "local-exec" {
     when        = destroy
     working_dir = "${path.module}/scripts"
-    command = "chmod +x loadBalancerIp_cluster_yaml_input.sh; ./loadBalancerIp_cluster_yaml_input.sh add $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
+    command = "chmod +x loadBalancerIp_cluster_yaml_input.sh; ./loadBalancerIp_cluster_yaml_input.sh rm $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
     environment = {
       CLUSTER_NAME     = self.triggers.cluster_name
       LOAD_BALANCER_IP = self.triggers.loadBalancerIp
@@ -69,13 +69,13 @@ resource "null_resource" "loadBalancerIp" {
 #   triggers = {
 #     cluster_name   = azurerm_kubernetes_cluster.aks_cluster.name
 #     service_mesh   = var.aks_cluster.service_mesh
-#     # loadBalancerIp = var.aks_cluster.loadBalancerIp != null ? var.aks_cluster.loadBalancerIp : ""
+#     loadBalancerIp = var.aks_cluster.loadBalancerIp
 #   }
 
 #   provisioner "local-exec" {
 #     when        = create
 #     working_dir = "${path.module}/scripts"
-#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh add $CLUSTER_NAME $SERVICE_MESH"
+#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh add $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
 #     environment = {
 #       CLUSTER_NAME     = self.triggers.cluster_name
 #       SERVICE_MESH     = self.triggers.service_mesh
@@ -89,7 +89,7 @@ resource "null_resource" "loadBalancerIp" {
 #   provisioner "local-exec" {
 #     when        = destroy
 #     working_dir = "${path.module}/scripts"
-#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh rm $CLUSTER_NAME $SERVICE_MESH"
+#     command     = "chmod +x service_mesh_cluster_yaml_input.sh; ./service_mesh_cluster_yaml_input.sh rm $CLUSTER_NAME \"$LOAD_BALANCER_IP\" \"$RESOURCE_GROUP\" \"$VNET_NAME\" \"$SUBNET_NAME\""
 #     environment = {
 #       CLUSTER_NAME     = self.triggers.cluster_name
 #       SERVICE_MESH     = self.triggers.service_mesh
