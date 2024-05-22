5c40e5fc1160f2da9f8984d187eecdbac9118e21
FouadDevOps <algahmif@aetna.com>
2024-05-22 13:50:53 -0400

    chnage vm size

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   default_node_pool {
     name       = var.node_pool_name
     node_count = var.node_count
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_B2s"
   }
 
   identity {
